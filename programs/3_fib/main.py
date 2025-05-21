e78ea8e732b26d779542744ffb7c479a05c257d7
Zackary <zspink@nea.k12.ar.us>
2025-05-21 11:21:54 -0500

    Ordered Apps

--- programs/3_fib/main.py
+++ programs/3_fib/main.py
@@ -6,9 +6,11 @@ def main():
     """Main function to run the Fibonacci program"""
 
     # fib_linear()
-    fib_braid()
+    # fib_braid()
+    fib_recursive()
 
 
+# Fibonacci using linear pattern
 def fib_linear():
     """Hardcoded colors of Fibonacci numbers using tkinter """
 
@@ -79,6 +81,44 @@ def get_row_col(i: int, c: int) -> tuple:
         col = 0
 
     return row,col
+
+
+# Fibonacci using recursion
+def fib_recursive() -> None:
+    """Hardcoded colors of Fibonacci numbers using tkinter using recursion"""
+
+    colors: list = ["red", "orange", "green", "blue", "darkblue", "purple"]
+
+    count: int = 12
+
+    wn: tk.Tk = tk.Tk()
+    wn.title("Fibonacci")
+    wn.config(bg="black")
+
+    for i in range(count):
+        c: int = get_fib(i)
+
+        if c == 0:
+            continue
+        
+        square: tk.Frame = tk.Frame(wn, width=c, height=c, bg=colors[i % len(colors)])
+        square.pack(side="left", anchor="s")
+        
+        sleep(0.5)
+        wn.update()
+
+    wn.mainloop()
+
+
+def get_fib(num: int) -> int:
+    """Recursive Fibonacci function"""
+
+    if num <= 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return get_fib(num - 1) + get_fib(num - 2)
 
 
 if __name__ == "__main__":
